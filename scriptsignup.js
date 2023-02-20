1017c57b1092215918d1af428d1128749d6ff9e6
raeygetahun <103925724+raeygetahun@users.noreply.github.com>
2023-02-20 20:31:39 +0100

    Update scriptsignup.js

--- scriptsignup.js
+++ scriptsignup.js
@@ -28,11 +28,7 @@
         //const analytics = getAnalytics(app);
         const database=getDatabase(app);
         const auth=getAuth();
-        const { getDatabase } = require('firebase-admin/database');
 
-        // Get a database reference to our blog
-        const db = getDatabase();
-        const ref = db.ref('server/saving-data/fireblog');
 
 
         //login registration 
